1a28417123701b9a80817f5229983b55337a1f61
Ayush Bisht <137602136+WebDev-Ayush@users.noreply.github.com>
2026-08-24 21:17:18 +0530

    Time: 3 ms (100%), Space: 83.4 MB (79.15%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.java
+++ 1872-stone-game-viii/1872-stone-game-viii.java
@@ -3,21 +3,14 @@ class Solution {
         int n = stones.length;
         int currentPrefixSum = 0;
         
-        // Calculate the total sum of the array, which is prefix[n-1]
         for (int stone : stones) {
             currentPrefixSum += stone;
         }
         
-        // Base case: If we must pick all stones (the only choice left)
         int maxDifference = currentPrefixSum;
         
-        // Work backwards from the second-to-last index down to 1
         for (int i = n - 2; i >= 1; i--) {
-            // Subtract stones[i + 1] from currentPrefixSum to get prefix[i]
             currentPrefixSum -= stones[i + 1];
-            
-            // Choose either to NOT pick at this index (keep maxDifference)
-            // or to PICK at this index (currentPrefixSum - maxDifference)
             maxDifference = Math.max(maxDifference, currentPrefixSum - maxDifference);
         }
         
